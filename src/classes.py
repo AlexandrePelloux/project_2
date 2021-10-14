446a2b1cda49e5eefbc275230ef9c52fd5d4c584
Alexandre <alexpmichelet@gmail.com>
2021-10-14 12:05:05 +0200

    Feat: First version of floor visual

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,7 @@
-from src.errors import *
+from errors import *
+import pygame 
+
+
 
 class Point():
     """ class representing the coordinates in 2D of a point.
@@ -109,6 +112,53 @@ class Floor(Area):
     def get_floor_nb(self):
         return self._floor_nb
 
+    def draw(self):
+        # initialize game ingine:
+        pygame.init()
+        # define some colors 
+        BLACK = (0,0,0)
+        WHITE = ( 255, 255, 255)
+        GREEN = ( 0, 255, 0)
+        RED = ( 255, 0, 0)
+        # open a new window 
+        size = (700,500)
+        screen = pygame.display.set_mode(size)
+        pygame.display.set_caption(f'This is the representation of the floor number {self._floor_nb}')
+        # The loop will carry on until the user exit the game (e.g. clicks the close button).
+        carryOn = True
+        
+        # The clock will be used to control how fast the screen updates
+        clock = pygame.time.Clock()
+        
+        
+        # -------- Main Program Loop -----------
+        while carryOn:
+            # --- Main event loop
+            for event in pygame.event.get(): # User did something
+                if event.type == pygame.QUIT: # If user clicked close
+                    carryOn = False # Flag that we are done so we exit this loop
+        
+            # --- Game logic should go here
+        
+            # --- Drawing code should go here
+            # First, clear the screen to white. 
+            screen.fill(WHITE)
+            #The you can draw different shapes and lines or add text to your background stage.
+            pygame.draw.rect(screen, RED, [self.bounding_box['c1'].x,self.bounding_box['c1'].y,self.bounding_box['c2'].x,self.bounding_box['c2'].y],5) # rectangle coordinates are represented by [c1.x,c1.y,c2.x,c2.y]
+            #pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+            #pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
+        
+        
+            # --- Go ahead and update the screen with what we've drawn.
+            pygame.display.flip()
+            
+            # --- Limit to 60 frames per second
+            
+            clock.tick(1)
+        
+        #Once we have exited the main program loop we can stop the game engine:
+        pygame.quit()
+
 class Room(Area):
     def __init__(self,bounding_box,walls):
         assert all(isinstance(wall,Wall) for wall in walls)
@@ -201,3 +251,11 @@ class Person():
             else: 
                 raise NotInBuildingError(f"Impossible to go upstairs, {self.name} is not in a building")
 
+
+
+if __name__=='__main__':
+    C1 = Point(50,0) # point en haut à gauche du rectangle (décalage de C1.x vers la droite, décalage de C1.y vers le bas)
+    C2 = Point(100,100) # point en bas à droite du rectangle 
+    bounding_box = {'c1':C1,'c2': C2}
+    test_floor  = Floor(bounding_box,4)
+    test_floor.draw()
